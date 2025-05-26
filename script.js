4ab509b7ff575be9d2bae342e050f2547f22a2e9
Keita-ati371 <erickeita@icloud.com>
2025-05-26 11:42:39 +0900

    Update script.js

--- script.js
+++ script.js
@@ -41,7 +41,7 @@ const dropDownUnit = document.getElementById("unit")
 
 dropDownUnit.addEventListener("change", () => {
   unitNumber = dropDownUnit.selectedOptions[0].value;
-})
+});
 
 task.addEventListener("change", () => {
   workTime = task.value
@@ -82,31 +82,38 @@ function calculateHoursBefore() {
     }
   }
   monthHourBefore = Math.round(monthHourBefore / 12 * 100) / 100;
-  stringsArr.push("年 ÷ 12カ月 = ", monthHourBefore, workTimeStr, "/月")
-  const str = stringsArr.join()
-  const arr = str.split(",")
+  stringsArr.push("年 ÷ 12カ月 = ", monthHourBefore, workTimeStr, "/月");
+  stringsArr.unshift("改善前：");
+  const str = stringsArr.join();
+  const arr = str.split(",");
   strings = arr.join("");
   if (workTime == 0 && workRe == 0 && oneTimeWork == 0 && pjt == 0 && otherWork == 0) {
     document.getElementById("resultBefore").innerText = "数値を入力してください。";
+    monthHourBefore = 0;
   } else if (workTime == 0) {
     document.getElementById("resultBefore").innerText = "工数を入力してください。";
+    monthHourBefore = 0;
   } else {
     document.getElementById("btnBefore").innerText = " 改善前再計算 "
     document.getElementById("resultBefore").innerText = strings;
     if (monthHourBefore > 0 && monthHourAfter > 0) {
-      document.getElementById("result").innerText = ["改善前：", monthHourBefore, workTimeStr, "/月 - 改善後：", monthHourAfter, workTimeStr, "/月 = ", monthHourBefore - monthHourAfter, workTimeStr, "/月の工数低減"].join("");
-       if (monthHourBefore - monthHourAfter >= 80) {
-        document.getElementById("resultPoint").innerText = "結果ポイント：５";
-      } else if (monthHourBefore - monthHourAfter >= 40) {
-        document.getElementById("resultPoint").innerText = "結果ポイント：４";
-      } else if (monthHourBefore - monthHourAfter >= 20) {
-        document.getElementById("resultPoint").innerText = "結果ポイント：３";
-      } else if (monthHourBefore - monthHourAfter >= 10) {
-        document.getElementById("resultPoint").innerText = "結果ポイント：２";
-      } else if (monthHourBefore - monthHourAfter >= 2) {
-        document.getElementById("resultPoint").innerText = "結果ポイント：１";
+      document.getElementById("result").innerText = ["改善前：", monthHourBefore, workTimeStr, "/月 - 改善後：", monthHourAfter, workTimeStr, "/月 = ", Math.round((monthHourBefore - monthHourAfter) * 100) / 100, workTimeStr, "/月の工数低減"].join("");
+      if (workTimeStr === "h") {
+        if (monthHourBefore - monthHourAfter >= 80) {
+          document.getElementById("resultPoint").innerText = "結果ポイント：５";
+        } else if (monthHourBefore - monthHourAfter >= 40) {
+          document.getElementById("resultPoint").innerText = "結果ポイント：４";
+        } else if (monthHourBefore - monthHourAfter >= 20) {
+          document.getElementById("resultPoint").innerText = "結果ポイント：３";
+        } else if (monthHourBefore - monthHourAfter >= 10) {
+          document.getElementById("resultPoint").innerText = "結果ポイント：２";
+        } else if (monthHourBefore - monthHourAfter >= 2) {
+          document.getElementById("resultPoint").innerText = "結果ポイント：１";
+        } else {
+          document.getElementById("resultPoint").innerText = "結果ポイント：０";
+        }
       } else {
-        document.getElementById("resultPoint").innerText = "結果ポイント：０";
+        document.getElementById("resultPoint").innerText = "";
       }
     }
   }
@@ -130,31 +137,38 @@ function calculateHoursAfter() {
     }
   }
   monthHourAfter = Math.round(monthHourAfter / 12 * 100) / 100;
-  stringsArr.push("年 ÷ 12カ月 = ", monthHourAfter, workTimeStr, "/月")
-  const str = stringsArr.join()
-  const arr = str.split(",")
+  stringsArr.push("年 ÷ 12カ月 = ", monthHourAfter, workTimeStr, "/月");
+  stringsArr.unshift("改善後：");
+  const str = stringsArr.join();
+  const arr = str.split(",");
   strings = arr.join("");
   if (workTime == 0 && workRe == 0 && oneTimeWork == 0 && pjt == 0 && otherWork == 0) {
     document.getElementById("resultAfter").innerText = "数値を入力してください。";
+    monthHourAfter = 0;
   } else if (workTime == 0) {
     document.getElementById("resultAfter").innerText = "工数を入力してください。";
+    monthHourAfter = 0;
   } else {
     document.getElementById("btnAfter").innerText = " 改善後再計算 "
     document.getElementById("resultAfter").innerText = strings;
     if (monthHourBefore > 0 && monthHourAfter > 0) {
-      document.getElementById("result").innerText = ["改善前：", monthHourBefore, workTimeStr, "/月 - 改善後：", monthHourAfter, workTimeStr, "/月 = ", monthHourBefore - monthHourAfter, workTimeStr, "/月の工数低減"].join("");
-      if (monthHourBefore - monthHourAfter >= 80) {
-        document.getElementById("resultPoint").innerText = "結果ポイント：５";
-      } else if (monthHourBefore - monthHourAfter >= 40) {
-        document.getElementById("resultPoint").innerText = "結果ポイント：４";
-      } else if (monthHourBefore - monthHourAfter >= 20) {
-        document.getElementById("resultPoint").innerText = "結果ポイント：３";
-      } else if (monthHourBefore - monthHourAfter >= 10) {
-        document.getElementById("resultPoint").innerText = "結果ポイント：２";
-      } else if (monthHourBefore - monthHourAfter >= 2) {
-        document.getElementById("resultPoint").innerText = "結果ポイント：１";
+      document.getElementById("result").innerText = ["改善前：", monthHourBefore, workTimeStr, "/月 - 改善後：", monthHourAfter, workTimeStr, "/月 = ", Math.round((monthHourBefore - monthHourAfter) * 100) / 100, workTimeStr, "/月の工数低減"].join("");
+      if (workTimeStr === "h") {
+        if (monthHourBefore - monthHourAfter >= 80) {
+          document.getElementById("resultPoint").innerText = "結果ポイント：５";
+        } else if (monthHourBefore - monthHourAfter >= 40) {
+          document.getElementById("resultPoint").innerText = "結果ポイント：４";
+        } else if (monthHourBefore - monthHourAfter >= 20) {
+          document.getElementById("resultPoint").innerText = "結果ポイント：３";
+        } else if (monthHourBefore - monthHourAfter >= 10) {
+          document.getElementById("resultPoint").innerText = "結果ポイント：２";
+        } else if (monthHourBefore - monthHourAfter >= 2) {
+          document.getElementById("resultPoint").innerText = "結果ポイント：１";
+        } else {
+          document.getElementById("resultPoint").innerText = "結果ポイント：０";
+        }
       } else {
-        document.getElementById("resultPoint").innerText = "結果ポイント：０";
+        document.getElementById("resultPoint").innerText = "";
       }
     }
   }
